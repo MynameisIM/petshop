4f27ff507ebfa077fa94e33cd3c14279a11a6b79
MynameisIM <accforyoutube@mail.ru>
2019-10-21 14:19:18 +0400

    Added modules

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -48,6 +48,7 @@
 </template>
 <script>
 import MyHeader from './Header.vue';
+import {mapGetters} from 'vuex';
 export default {
   name: 'imain',
   data() {
@@ -97,9 +98,9 @@ export default {
         return productsArray.sort(compare);
       }
     },
-    products() {
-      return this.$store.getters.products;
-    }
+    ...mapGetters([
+        'products'
+      ])
   },
   filters: {
     formatPrice(price) {
